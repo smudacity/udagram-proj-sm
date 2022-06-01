39f596c1bcd3c70d7528f339fae99a6d9287d1f6
Sricon <sricon@gmail.com>
2022-06-01 23:21:16 +0100

    screenshots

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aa3ec009056164c178d8a25858bda382-701661751.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
