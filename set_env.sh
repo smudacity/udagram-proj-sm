21c3973b1dbd40934150c5f21594f1b61b8ab3c6
Sricon <sricon@gmail.com>
2022-06-01 17:16:58 +0100

    re init

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres_sm
+export POSTGRES_PASSWORD=postgres_sm
+export POSTGRES_HOST=udagram-eks-proj-sm.ccqbiztzl8dc.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-eks-proj-sm
+export AWS_REGION=us-east-1
+export AWS_PROFILE=awsCliEKS
+export JWT_SECRET=mysecret
+export URL=http://localhost:8100
